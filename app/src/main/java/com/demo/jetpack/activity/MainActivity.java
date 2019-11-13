637972552fb59680df9cc8643ece7abcd054949d
Administrator <390632594@qq.com>
2019-11-13 19:48:02 +0800

    bottom气泡提示

--- app/src/main/java/com/demo/jetpack/activity/MainActivity.java
+++ app/src/main/java/com/demo/jetpack/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.demo.jetpack.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.demo.jetpack.R;
@@ -12,22 +13,17 @@ import androidx.annotation.Nullable;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
+import q.rorbin.badgeview.Badge;
+import q.rorbin.badgeview.QBadgeView;
 
 public class MainActivity extends BaseActivity {
     private long lastTime = 0;
     private static final String TAG = "MainActivity";
+    private BottomNavigationViewEx mBottomNav;
+
     @Override
     public int getLayout() {
         return R.layout.activity_main;
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (System.currentTimeMillis() - lastTime > 1500) {
-            lastTime = System.currentTimeMillis();
-            Toast.makeText(MainActivity.this, "再按一次退出", Toast.LENGTH_SHORT).show();
-        } else
-            super.onBackPressed();
     }
 
     @Override
@@ -42,8 +38,35 @@ public class MainActivity extends BaseActivity {
 
     private void initBottomTab() {
         NavController navController = Navigation.findNavController(this, R.id.my_nav_host_fragment);
-        BottomNavigationViewEx bottomNav = findViewById(R.id.bnv_bottom_nav);
-        bottomNav.enableAnimation(false);
-        NavigationUI.setupWithNavController(bottomNav, navController);
+        mBottomNav = findViewById(R.id.bnv_bottom_nav);
+        mBottomNav.enableAnimation(false);
+        NavigationUI.setupWithNavController(mBottomNav, navController);
+
+        addBadgeAt(2,99);
     }
+
+    private Badge addBadgeAt(int position, int number) {
+        // add badge
+        return new QBadgeView(this)
+                .setBadgeNumber(number)
+                .setGravityOffset(36, 2, true)
+                .bindTarget(mBottomNav.getBottomNavigationItemView(position))
+                .setOnDragStateChangedListener(new Badge.OnDragStateChangedListener() {
+                    @Override
+                    public void onDragStateChanged(int dragState, Badge badge, View targetView) {
+                        if (Badge.OnDragStateChangedListener.STATE_SUCCEED == dragState)
+                            Toast.makeText(MainActivity.this, "Ddd", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (System.currentTimeMillis() - lastTime > 1500) {
+            lastTime = System.currentTimeMillis();
+            Toast.makeText(MainActivity.this, "再按一次退出", Toast.LENGTH_SHORT).show();
+        } else
+            super.onBackPressed();
+    }
+
 }
--- app/src/main/java/com/demo/jetpack/viewmodel/NumViewModel.java
+++ app/src/main/java/com/demo/jetpack/viewmodel/NumViewModel.java
@@ -1,6 +1,15 @@
 package com.demo.jetpack.viewmodel;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class NumViewModel extends ViewModel {
+    private MutableLiveData<Integer> num;
+
+    public MutableLiveData<Integer> getNum() {
+        if (num == null) {
+            num = new MutableLiveData<>();
+        }
+        return num;
+    }
 }
